672377a18729890d71198f9bac3b32dc995dc31f
hanYijin <gksdlwls123@naver.com>
2021-06-04 10:02:29 +0900

    r

--- AI/data0515.R
+++
@@ -1,9 +0,0 @@
-library(devtools)
-#install_github('dkahle/ggmap')
-library(ggmap)
-
-apikey = 'AIzaSyD7gpTZYkQdVsLaSKNpS-70gGN0IYIYlck'
-
-register_google(apikey)
-ggDaegu <- get_googlemap('daegu', maptype='hybrid')
-ggmap(ggDaegu)
